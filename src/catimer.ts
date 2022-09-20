46e1572bb96d6f0916e63d2f601d24e707a2830c
Fatima Qarni <fatimaqarni@microsoft.com>
2022-09-20 14:43:29 -0700

    add placeholder for status bar summary

--- src/catimer.ts
+++ src/catimer.ts
@@ -1,4 +1,4 @@
-class catimer {
+export class catimer {
 
     private work_length: Number;
     private break_length: Number;
@@ -8,15 +8,48 @@ class catimer {
     private max_sessions: Number;
     private task_name: String;
 
+    private status: Boolean; // paused == F or working == T
+    private time: Number;   // TODO: update this to be an actual timer
+
+
     public constructor() {
         this.work_length = 25;
         this.break_length = 5;
         this.long_break_length = 20;
 
-        this.session_number = 0;
+        this.session_number = 1;
         this.max_sessions = 4;
         this.task_name = "";
+
+        this.status = false;
+        this.time = this.work_length;
     }
 
+    public get workLength() : Number {
+        return this.work_length;
+    }
+    public get breakLength() : Number {
+        return this.break_length;
+    }
+    public get longBreakLength() : Number {
+        return this.long_break_length;
+    }
 
+    public get sessionNumber() : Number {
+        return this.session_number;
+    }
+    public get maxSessions() : Number {
+        return this.max_sessions;
+    }
+        public get taskName() : String {
+        return this.task_name;
+    }
+
+    public get currStatus() : Boolean {
+        return this.status;
+    }
+    public get timeRemaining() : Number {
+        return this.time;
+    }
+    
 }
