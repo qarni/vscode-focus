a2f5e958669ba0bcd7bbcd3a7555b60e0ea724e1
Peter Nie <peternie@microsoft.com>
2022-11-06 21:08:44 -0800

    timer without pause

--- src/catimer.ts
+++ src/catimer.ts
@@ -1,71 +1,74 @@
 export class catimer {
-  private work_length: Number;
-  private break_length: Number;
-  private long_break_length: Number;
 
-  private session_number: Number;
-  private max_sessions: Number;
+  private work_length: number;
+  private break_length: number;
+  private long_break_length: number;
+
+  private session_number: number;
+  private max_sessions: number;
   private task_name: String;
 
   private is_running: Boolean; // paused == F or working == T
-  private time: Number; // TODO: update this to be an actual timer
+  private minutes_remaining: number;
+  private seconds_remianing: number;
+  private session_name : String; //work, break, long break
 
   public constructor() {
-    this.work_length = 25;
-    this.break_length = 5;
-    this.long_break_length = 20;
+      this.work_length = 25;
+      this.break_length = 5;
+      this.long_break_length = 15;
 
-    this.session_number = 1;
-    this.max_sessions = 4;
-    this.task_name = "";
+      this.session_number = 1;
+      this.max_sessions = 4;
+      this.task_name = "";
 
-    this.is_running = false;
-    this.time = this.work_length;
-  }
-
-  public get workLength(): Number {
-    return this.work_length;
-  }
-  public get breakLength(): Number {
-    return this.break_length;
-  }
-  public get longBreakLength(): Number {
-    return this.long_break_length;
+      this.is_running = false;
+      this.minutes_remaining = this.work_length;
+      this.seconds_remianing = 0;
+      this.session_name = "work";
   }
 
-  public get sessionNumber(): Number {
-    return this.session_number;
+  public get workLength() : number {
+      return this.work_length;
   }
-  public get maxSessions(): Number {
-    return this.max_sessions;
+  public get breakLength() : number {
+      return this.break_length;
   }
-  public get taskName(): String {
-    return this.task_name;
-  }
-
-  public get isRunning(): Boolean {
-    return this.is_running;
-  }
-  public get timeRemaining(): Number {
-    return this.time;
+  public get longBreakLength() : number {
+      return this.long_break_length;
   }
 
-  public set workLength(workLength: Number) {
-    this.work_length = workLength;
+  public get sessionNumber() : number {
+      return this.session_number;
   }
-  public set breakLength(breakLength: Number) {
-    this.break_length = breakLength;
+  public get maxSessions() : number {
+      return this.max_sessions;
   }
-  public set longBreakLength(longBreakLength: Number) {
-    this.long_break_length = longBreakLength;
+  public get taskName() : String {
+      return this.task_name;
   }
-  public set maxSessions(maxSessions: Number) {
-    this.max_sessions = maxSessions;
+
+  public get isRunning() : Boolean {
+      return this.is_running;
   }
-  public set setTaskName(name: String) {
-    this.task_name = name;
+  public get timeRemaining() : String {
+      return this.minutes_remaining + ":" + this.seconds_remianing;
   }
-  public changeStatus() {
-    this.is_running = !this.is_running;
+  public get sessionName() : String {
+      return this.session_name;
+  }
+
+  public set setTaskName(name : String) {
+      this.task_name = name;
+  }
+  public changeStatus () {
+      this.is_running = !this.is_running;
+  }
+  public setTimeRemaining (min : number, sec : number) {
+      this.minutes_remaining = min;
+      this.seconds_remianing = sec;
+  }
+  public setSessionName (newSessionName : String) {
+      this.session_name = newSessionName;
   }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,96 +1,108 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
-import { getConfigValue, ThemeConfig, updateConfig } from "./config/config";
+import * as vscode from 'vscode';
 
-import { catimer } from "./catimer";
+import {catimer} from './catimer';
 
 let statusBarItem: vscode.StatusBarItem;
 let catTimer: catimer;
+let counter : { min: number, sec : number };
+let sub_session_count : number;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "focus" is now active!');
+	
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "focus" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let startFocus = vscode.commands.registerCommand("focus.startFocus", () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Time to Focus!");
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let startFocus = vscode.commands.registerCommand('focus.startFocus', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Time to Focus!');
 
-    // call constructor & start focus session
-  });
+		// call constructor & start focus session
+	});
 
-  // initialize timer
-  catTimer = new catimer();
-  // create a new status bar item that we can now manage
-  statusBarItem = vscode.window.createStatusBarItem(
-    vscode.StatusBarAlignment.Right,
-    100
-  );
+	// initialize timer 
+	catTimer = new catimer();
+	// create a new status bar item that we can now manage
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
 
-  let showIcon = vscode.commands.registerCommand("focus.showIcon", () => {
-    // call the start focus functi
-    let options: vscode.InputBoxOptions = {
-      prompt: "Type in your task name to start the focus session! (*≧ω≦)",
-    };
+	sub_session_count = 0;
 
-    vscode.window.showInputBox(options).then((value: any) => {
-      if (!value) return;
-      catTimer.setTaskName = value;
-      catTimer.changeStatus;
-      updateStatusBar();
-    });
-  });
+	let showIcon = vscode.commands.registerCommand('focus.showIcon', () => {
+		// call the start focus functi
+		let options: vscode.InputBoxOptions = {
+			prompt: "Type in your task name to start the focus session! (*≧ω≦)",
+		};
+		
+		vscode.window.showInputBox(options).then(async (value: any) => {
+			if (!value) return;
+			catTimer.setTaskName = value;
+			catTimer.changeStatus;
+			updateStatusBar();
+			startInfinitetimer();
+		});
+	});
 
-  context.subscriptions.push(startFocus);
-  context.subscriptions.push(showIcon);
-  context.subscriptions.push(statusBarItem);
+	context.subscriptions.push(startFocus);
+	context.subscriptions.push(showIcon);
+	context.subscriptions.push(statusBarItem);
+}
 
-  // Subscribe to configuration changes
-  context.subscriptions.push(
-    vscode.workspace.onDidChangeConfiguration(onDidChangeConfiguration)
-  );
 
-  // Initialize from the current configuration
-  onDidChangeConfiguration();
+async function startInfinitetimer() {
+	while (true) {
+		for (let i = 0; i < catTimer.maxSessions; i++) {
+			startTimer(catTimer.workLength, 0);
+			await new Promise((resolve, reject) => setTimeout(resolve, 1000 * catTimer.workLength * 60));
+			startTimer(catTimer.breakLength, 0);
+			await new Promise((resolve, reject) => setTimeout(resolve, 1000 * catTimer.breakLength * 60));
+
+		}
+		startTimer(catTimer.longBreakLength, 0);
+		await new Promise((resolve, reject) => setTimeout(resolve, 1000 * catTimer.longBreakLength * 60));
+	}
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
-  statusBarItem.dispose();
+	statusBarItem.dispose();
 }
 
-function onDidChangeConfiguration() {
-  const config = vscode.workspace.getConfiguration("focus");
-  const workLengthSetting = getConfigValue("work_length_setting", config);
-  const breakLengthSetting = getConfigValue("break_length_setting", config);
-  const longBreakLengthSetting = getConfigValue(
-    "long_break_length_setting",
-    config
-  );
-  const maxSessionsSetting = getConfigValue("max_sessions_setting", config);
-  catTimer.workLength = workLengthSetting;
-  catTimer.breakLength = breakLengthSetting;
-  catTimer.breakLength = longBreakLengthSetting;
-  catTimer.breakLength = maxSessionsSetting;
+function startTimer (minutes : number, seconds : number) {
+	counter = { min: minutes, sec: 0 };
+	let intervalId = setInterval(() => {
+		if (counter.sec - 1 === -1) {
+			counter.min -= 1;
+			counter.sec = 59;
+		} else {
+			counter.sec -= 1;
+		}
+		if (counter.min === 0 && counter.sec === 0) {
+			clearInterval(intervalId);
+		}
+		catTimer.setTimeRemaining(counter.min, counter.sec);
+		updateStatusBar();
+	}, 1000);
+  }
 
-  // Test to check if settings are populating and being passed downstream.
-  vscode.commands.executeCommand("workbench.action.openSettings", "focus");
-}
-
-// Called every time the status bar needs to be updated
+  // Called every time the status bar needs to be updated
 function updateStatusBar(): void {
-  let statusSymbol = catTimer.isRunning ? "$(play)" : "$(debug-pause)";
-  statusBarItem.text =
-    `${catTimer.timeRemaining} ` +
-    statusSymbol +
-    `Session: ${catTimer.sessionNumber}/${catTimer.maxSessions} ` +
-    `Task: ${catTimer.taskName}`;
-  statusBarItem.show();
+	let statusSymbol = catTimer.isRunning ? '$(play)' : '$(debug-pause)';
+	statusBarItem.text = `Remained ${catTimer.sessionName} time: ` + 
+						`${catTimer.timeRemaining} ` +
+						statusSymbol + 
+						`Session: ${catTimer.sessionNumber}/${catTimer.maxSessions} ` +
+						`Task: ${catTimer.taskName}`;
+	statusBarItem.show();
+}
+
+function delay(arg0: number) {
+	throw new Error('Function not implemented.');
 }
