adbf815c16e2a24058a97503d60c906cae16ba20
Fatima Qarni <fatimaqarni@microsoft.com>
2022-11-08 14:05:29 -0800

    button

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,17 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import {catimer} from './catimer';
+import { catimer } from './catimer';
 
 let statusBarItem: vscode.StatusBarItem;
 let catTimer: catimer;
-let counter : { min: number, sec : number };
-let sub_session_count : number;
+let counter: { min: number, sec: number };
+let sub_session_count: number;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "focus" is now active!');
@@ -30,8 +30,19 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// initialize timer 
 	catTimer = new catimer();
+
+	// create status bar and command to click on it
+	const statusBarClick = 'sample.showSelectionCount';
+	context.subscriptions.push(vscode.commands.registerCommand(statusBarClick, () => {
+		catTimer.changeStatus();
+		vscode.window.showInformationMessage(`Cat timer is now ` + (catTimer.isPaused ? 'paused' : 'resumed'));
+
+		updateStatusBar();
+	}));
 	// create a new status bar item that we can now manage
 	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	statusBarItem.command = statusBarClick;
+
 
 	sub_session_count = 0;
 
@@ -40,7 +51,7 @@ export function activate(context: vscode.ExtensionContext) {
 		let options: vscode.InputBoxOptions = {
 			prompt: "Type in your task name to start the focus session! (*≧ω≦)",
 		};
-		
+
 		vscode.window.showInputBox(options).then(async (value: any) => {
 			if (!value) return;
 			catTimer.setTaskName = value;
@@ -50,8 +61,8 @@ export function activate(context: vscode.ExtensionContext) {
 		});
 	});
 
-  // TODO
-  // Add the call when click pause. change the catTimer.paused to change the boolean. click it twice --> call the starttimer
+	// TODO
+	// Add the call when click pause. change the catTimer.paused to change the boolean. click it twice --> call the starttimer
 
 	context.subscriptions.push(startFocus);
 	context.subscriptions.push(showIcon);
@@ -60,29 +71,29 @@ export function activate(context: vscode.ExtensionContext) {
 
 
 async function startInfinitetimer() {
-  // start from the given time 
-  while(catTimer.isPaused) {
-    //finish the current paused session or the brand new session
-    startTimer(catTimer.remainingMin, catTimer.remainingSec);
-    await new Promise((resolve, reject) => setTimeout(resolve, 1000 * (catTimer.remainingMin * 60 + catTimer.remainingSec)));
-    // finish the rest sessions
-    for (let i = catTimer.sessionNumber; i < catTimer.maxSessions; i++) {
-      catTimer.increaseSessionNumber(i);
-      if (i % 2 === 1) {
-        // WORK
-        startTimer(catTimer.workLength, 0);
-        await new Promise((resolve, reject) => setTimeout(resolve, 1000 * catTimer.workLength * 60));
-      } else {
-        // BREAK
-        startTimer(catTimer.breakLength, 0);
-        await new Promise((resolve, reject) => setTimeout(resolve, 1000 * catTimer.breakLength * 60));
-      }
-    }
-    // long break
-    catTimer.increaseSessionNumber(1); // set the session back to 1. 
-    startTimer(catTimer.longBreakLength, 0);
-    await new Promise((resolve, reject) => setTimeout(resolve, 1000 * catTimer.longBreakLength * 60));
-  }
+	// start from the given time 
+	while (catTimer.isPaused) {
+		//finish the current paused session or the brand new session
+		startTimer(catTimer.remainingMin, catTimer.remainingSec);
+		await new Promise((resolve, reject) => setTimeout(resolve, 1000 * (catTimer.remainingMin * 60 + catTimer.remainingSec)));
+		// finish the rest sessions
+		for (let i = catTimer.sessionNumber; i < catTimer.maxSessions; i++) {
+			catTimer.increaseSessionNumber(i);
+			if (i % 2 === 1) {
+				// WORK
+				startTimer(catTimer.workLength, 0);
+				await new Promise((resolve, reject) => setTimeout(resolve, 1000 * catTimer.workLength * 60));
+			} else {
+				// BREAK
+				startTimer(catTimer.breakLength, 0);
+				await new Promise((resolve, reject) => setTimeout(resolve, 1000 * catTimer.breakLength * 60));
+			}
+		}
+		// long break
+		catTimer.increaseSessionNumber(1); // set the session back to 1. 
+		startTimer(catTimer.longBreakLength, 0);
+		await new Promise((resolve, reject) => setTimeout(resolve, 1000 * catTimer.longBreakLength * 60));
+	}
 }
 
 // this method is called when your extension is deactivated
@@ -90,7 +101,7 @@ export function deactivate() {
 	statusBarItem.dispose();
 }
 
-function startTimer (minutes : number, seconds : number) {
+function startTimer(minutes: number, seconds: number) {
 	counter = { min: minutes, sec: 0 };
 	let intervalId = setInterval(() => {
 		if (counter.sec - 1 === -1) {
@@ -103,20 +114,20 @@ function startTimer (minutes : number, seconds : number) {
 		if (counter.min === 0 && counter.sec === 0) {
 			clearInterval(intervalId);
 		}
-    // keep on setting remaining time 
+		// keep on setting remaining time 
 		catTimer.setTimeRemaining(counter.min, counter.sec);
 		updateStatusBar();
 	}, 1000);
-  }
+}
 
-  // Called every time the status bar needs to be updated
+// Called every time the status bar needs to be updated
 function updateStatusBar(): void {
 	let statusSymbol = catTimer.isPaused ? '$(play)' : '$(debug-pause)';
-	statusBarItem.text = `Remained ${catTimer.sessionName} time: ` + 
-						`${catTimer.timeRemaining} ` +
-						statusSymbol + 
-						`Session: ${catTimer.sessionNumber}/${catTimer.maxSessions} ` +
-						`Task: ${catTimer.taskName}`;
+	statusBarItem.text = statusSymbol +
+		`Remaining time: ` +
+		`${catTimer.timeRemaining} ` +
+		`Session: ${catTimer.sessionNumber}/${catTimer.maxSessions} ` +
+		`Task: ${catTimer.taskName}`;
 	statusBarItem.show();
 }
 
