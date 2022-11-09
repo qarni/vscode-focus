9dc8cf5d14195ebd54063361832a43b4e199b361
Fatima Qarni <fatima.q832@gmail.com>
2022-11-08 17:13:19 -0800

    Revert "Button for playpause + peter's work too"

--- src/catimer.ts
+++ src/catimer.ts
@@ -1,83 +1,71 @@
 export class catimer {
+  private work_length: Number;
+  private break_length: Number;
+  private long_break_length: Number;
 
-  private work_length: number;
-  private break_length: number;
-  private long_break_length: number;
-
-  private session_number: number;
-  private max_sessions: number;
+  private session_number: Number;
+  private max_sessions: Number;
   private task_name: String;
 
-  private paused: Boolean; // paused == F or working == T
-  private minutes_remaining: number;
-  private seconds_remianing: number;
-  private session_name : String; //work, break, long break
+  private is_running: Boolean; // paused == F or working == T
+  private time: Number; // TODO: update this to be an actual timer
 
   public constructor() {
-      this.work_length = 25;
-      this.break_length = 5;
-      this.long_break_length = 15;
+    this.work_length = 25;
+    this.break_length = 5;
+    this.long_break_length = 20;
 
-      this.session_number = 1;
-      this.max_sessions = 4;
-      this.task_name = "";
+    this.session_number = 1;
+    this.max_sessions = 4;
+    this.task_name = "";
 
-      this.paused = false;
-      this.minutes_remaining = this.work_length;
-      this.seconds_remianing = 0;
-      this.session_name = "work";
+    this.is_running = false;
+    this.time = this.work_length;
   }
 
-  public get workLength() : number {
-      return this.work_length;
+  public get workLength(): Number {
+    return this.work_length;
   }
-  public get breakLength() : number {
-      return this.break_length;
+  public get breakLength(): Number {
+    return this.break_length;
   }
-  public get longBreakLength() : number {
-      return this.long_break_length;
-  }
-
-  public get sessionNumber() : number {
-      return this.session_number;
-  }
-  public get maxSessions() : number {
-      return this.max_sessions;
-  }
-  public get taskName() : String {
-      return this.task_name;
+  public get longBreakLength(): Number {
+    return this.long_break_length;
   }
 
-  public get isPaused() : Boolean {
-      return this.paused;
+  public get sessionNumber(): Number {
+    return this.session_number;
   }
-  public get timeRemaining() : String {
-      return this.minutes_remaining + ":" + this.seconds_remianing;
+  public get maxSessions(): Number {
+    return this.max_sessions;
   }
-  public get sessionName() : String {
-      return this.session_name;
-  }
-  public get remainingMin () {
-      return this.minutes_remaining;
-  }
-  public get remainingSec () {
-      return this.seconds_remianing;
+  public get taskName(): String {
+    return this.task_name;
   }
 
-  public set setTaskName(name : String) {
-      this.task_name = name;
+  public get isRunning(): Boolean {
+    return this.is_running;
   }
-  public changeStatus () {
-      this.paused = !this.paused;
+  public get timeRemaining(): Number {
+    return this.time;
   }
-  public setTimeRemaining (min : number, sec : number) {
-      this.minutes_remaining = min;
-      this.seconds_remianing = sec;
+
+  public set workLength(workLength: Number) {
+    this.work_length = workLength;
   }
-  public setSessionName (newSessionName : String) {
-      this.session_name = newSessionName;
+  public set breakLength(breakLength: Number) {
+    this.break_length = breakLength;
   }
-  public increaseSessionNumber (newNum : number) {
-      this.session_number = newNum;
+  public set longBreakLength(longBreakLength: Number) {
+    this.long_break_length = longBreakLength;
+  }
+  public set maxSessions(maxSessions: Number) {
+    this.max_sessions = maxSessions;
+  }
+  public set setTaskName(name: String) {
+    this.task_name = name;
+  }
+  public changeStatus() {
+    this.is_running = !this.is_running;
   }
 }
